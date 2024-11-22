160a6b918c6251a902618710c2923900230b5e49
Amanda <00001108025882sp@al.educacao.sp.gov.br>
2024-11-21 21:37:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fisica',
+    'Quantas cores tem o arco-íris?',
+    'Sete'
+)
+
+criaCartao(
+    'Astrologia',
+    'Qual é o nome do único satélite natural da Terra?',
+    'A Lua'
+)
+
+criaCartao(
+    'Artes',
+    'As cores vermelho e azul misturadas formam qual cor?',
+    'Roxo'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a fórmula da água?',
+    'H2O'
+)
